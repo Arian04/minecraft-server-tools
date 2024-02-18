9b11698a450968742a40143426a56fb840c4a9d2
Arian Baishya <arian.baishya2004@gmail.com>
2024-02-17 23:53:15 -0600

    clean up server start script and Dockerfile

--- server/runtime/start.sh
+++ server/runtime/start.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-# Minecraft server start script
+main() {
+	# directory storing all the scripts below that will be called
+	BIN=${MC_BIN_DIR:?}
 
-# TODO: check for all required env vars
-: "${JAR_NAME:?}"
-: "${MEMORY_AMOUNT:?}"
+	# Check that all required env vars are set (doesn't currently check if they're valid though)
+	: "${JAR_NAME:?}"
+	: "${MEMORY_AMOUNT:?}"
 
-# start server
-# shellcheck disable=SC2086 # jvm_args word-splitting is intentional
-#java "-Xms${MEMORY_AMOUNT}" "-Xmx${MEMORY_AMOUNT}" ${jvm_args} --add-modules=jdk.incubator.vector -jar "${JAR_NAME}" nogui
+	# Run preparation script, if it returns an error, exit
+	"$BIN/prepare.py"
+
+	# Run actual server start script
+	"$BIN/server-start.sh"
+}
+
+main "$@"
