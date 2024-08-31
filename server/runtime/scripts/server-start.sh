2a2f046b7c2ad0ae6032ca986dfa07119a596217
Arian Baishya <arian.baishya2004@gmail.com>
2024-08-31 18:07:49 -0500

    server: disable option to use G1GC over ZGC
    I will never use that option and don't think anyone else will either

--- server/runtime/scripts/server-start.sh
+++ server/runtime/scripts/server-start.sh
@@ -5,29 +5,17 @@
 
 : "${JAR_NAME:?}"
 : "${MEMORY_AMOUNT:?}"
-: "${GC_TYPE:=G1GC}"
 
 # TODO: refactor so that you can easily override JVM args as a user of the container
+#: "${JVM_ARGS:?}"
 
 set_base_args() {
 	base_args='-XX:+UnlockExperimentalVMOptions -XX:+UnlockDiagnosticVMOptions -XX:+AlwaysActAsServerClassMachine -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseNUMA -XX:NmethodSweepActivity=1 -XX:ReservedCodeCacheSize=400M -XX:NonNMethodCodeHeapSize=12M -XX:ProfiledCodeHeapSize=194M -XX:NonProfiledCodeHeapSize=194M -XX:-DontCompileHugeMethods -XX:MaxNodeLimit=240000 -XX:NodeLimitFudgeFactor=8000 -XX:+UseVectorCmov -XX:+PerfDisableSharedMem -XX:+UseFastUnorderedTimeStamps -XX:+UseCriticalJavaThreadPriority -XX:ThreadPriorityPolicy=1 -XX:AllocatePrefetchStyle=3'
 }
 
 set_gc_args() {
-	readonly gc_type="${1:?}"
-
-	case "$gc_type" in
-	"G1GC")
-		gc_args='-XX:+UseG1GC -XX:MaxGCPauseMillis=130 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=28 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=20 -XX:G1MixedGCCountTarget=3 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=0 -XX:SurvivorRatio=32 -XX:MaxTenuringThreshold=1 -XX:G1SATBBufferEnqueueingThresholdPercent=30 -XX:G1ConcMarkStepDurationMillis=5 -XX:G1ConcRSHotCardLimit=16 -XX:G1ConcRefinementServiceIntervalMillis=150'
-		;;
-	"ZGC")
-		gc_args='-XX:+UseZGC -XX:AllocatePrefetchStyle=1 -XX:-ZProactive'
-		;;
-	*)
-		echo "ERROR: Unsupported GC_TYPE passed in server-start.sh"
-		exit 1
-		;;
-	esac
+	# Using ZGC
+	gc_args='-XX:+UseZGC -XX:AllocatePrefetchStyle=1 -XX:-ZProactive'
 
 	# NOTE:
 	# According to source linked at the top of this file, set ConcGCThreads = 2 on CPUs with 2c/4t and [# cores - 2] on most other CPUs.
@@ -49,7 +37,7 @@ set_extra_args() {
 main() {
 	set_base_args
 
-	set_gc_args "${GC_TYPE}"
+	set_gc_args
 
 	set_extra_args
 
